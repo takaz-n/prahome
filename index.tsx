68a73e15a246cd17fd1c0eb2f84ab01064a41c85
NT <bo1122f@gmail.com>
2024-06-13 09:34:03 +0900

    インデックス

---
+++ index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AppMain from './Main.tsx';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AppMain />
+  </React.StrictMode>
+);
