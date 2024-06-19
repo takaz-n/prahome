56bfcfdd2435dac160192808cafc5d5c82ec261d
tana4242 <ryota_tanabe-7020@jouhou-giken.info>
2024-06-19 11:22:57 +0900

    微修正

--- FLONT/src/main.tsx
+++ FLONT/src/main.tsx
@@ -11,5 +11,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Route path="/sub"element={<Sub/>}/>
       </Routes>
     </Router>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
