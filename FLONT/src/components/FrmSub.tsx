cd65480b3433e28429e1dfa743ca4d544e9525d5
NT <bo1122f@gmail.com>
2024-06-20 09:18:32 +0900

    一回目

--- FLONT/src/components/FrmSub.tsx
+++ FLONT/src/components/FrmSub.tsx
@@ -1,3 +1,34 @@
-export default function Sub(){
-    return(<div></div>)
+import { useEffect, useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom"; 
+import { TextField, Button } from "@mui/material" ;
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import zod from "zod";
+
+type Types = {
+    CODE: number;
+    NAME: string;
+};
+
+type AddTypes = {
+    CODE: number;
+    NAME: string;
+};
+
+
+
+const Sub: React.FC = () => {
+    const location = useLocation();
+    const navigate = useNavigate();
+    const { register, handleSubmit, reset } = useForm<AddTypes>;
+     
+
+
+
+    return(
+    <div>
+        
+
+    </div>
+    )
 }
