aa8c51acc45a59be69cbaea4c2afb7de849043e2
NT <bo1122f@gmail.com>
2024-06-24 10:22:10 +0900

    コメント文含む更新

--- FLONT/src/components/FrmSub.tsx
+++ FLONT/src/components/FrmSub.tsx
@@ -3,13 +3,13 @@ import { useNavigate, useLocation } from "react-router-dom";
 import {
   TextField,
   Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-  Grid,
-  Box,
-  Switch,
+  Dialog, //パスワード認証画面のダイアログボックスを構成する
+  DialogActions, //送信ボタンを配置する
+  DialogContent, //ダイアログの説明文などのメインコンテンツを設定
+  DialogTitle, //ダイアログのタイトル部分を表す
+  Grid, //テキストフィールドやボタンなどのアイテムの行と列のレイアウトを管理
+  Box, //テキストフィールドの広さやボタンの色などのスタイルを設定、適用
+  Switch, //トグルスイッチを提供
 } from "@mui/material";
 import { useForm } from "react-hook-form";
 import axios from "axios";
@@ -21,31 +21,37 @@ const Sub: React.FC = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const { handleSubmit } = useForm();
-  const { code, name } = location.state || {};
+  const { code, name } = location.state || {}; //frmMainからcode,nameのステートを受け取る
 
-  let [textC, setTextC] = useState<string>(code || "");
+  let   [textC, setTextC] = useState<string>(code || ""); //後にtextCの値に変化があるためletで定義
   const [textN, setTextN] = useState<string>(name || "");
   const [isEdit, setIsEdit] = useState(false);
-  useEffect(() => {
+  const [show, setShow] = useState(false); //パスワード入力画面表示の有無
+  const [password, setPassword] = useState(""); //入力されたパスワードの値を記憶
+  const [isOK, setisOK] = useState(false); //パスワードが合致してるかどうか
+  const [pic, setpic] = useState(nsx); //画面の状態管理
+  const h1 = pic === nsx ? "#ff9830" : "#ffffff"; //ヘッダーの色に基づいて設定
+  
+  useEffect(() => { //textCに値が存在していた場合に更新処理をfalse
     if (textC === code) {
       setIsEdit(false);
     } else {
       setIsEdit(true);
     }
   }, [textC]);
-  const addDatas = async () => {
+  const addDatas = async () => { //textCまたはtextNの入力フィールドが空だった場合に処理を中止する
     if (!textC || !textN) {
       alert("コードまたは名前を入力してください");
       return;
     }
     while (textC.length < 4) {
-      textC = "0" + textC;
+      textC = "0" + textC;  //textCが4文字未満の場合、先頭に0を追加
     }
     const response = await axios.post("http://localhost:3000/api/post/page", {
       data: { code: textC, name: textN },
     });
-    console.log(response.data.data.affectedRows);
-    if (response.data.data.affectedRows === 0) {
+    console.log(response.data.data.affectedRows); 
+    if (response.data.data.affectedRows === 0) { //返ってきた追加した行のデータが0だった場合アラートを表示
       alert("登録が失敗しました");
     } else {
       alert("登録が成功しました");
@@ -83,11 +89,6 @@ const Sub: React.FC = () => {
     }
   };
 
-  const [show, setShow] = useState(false); //パスワード入力画面表示の有無
-  const [password, setPassword] = useState(""); //入力されたパスワードの値を記憶
-  const [isOK, setisOK] = useState(false); //パスワードが合致してるかどうか
-  const [pic, setpic] = useState(nsx);
-  const h1 = pic === nsx ? "#ff9830" : "#ffffff";
   const handlePasswordSubmit = async () => {
     try {
       const response = await axios.post("http://localhost:3000/api/page/pass", {
@@ -95,7 +96,7 @@ const Sub: React.FC = () => {
       });
       if (response.data.success) {
         setisOK(true);
-        // 削除処理をここに追加
+        // パスワードが合っていたら削除処理をここで実行
         deleteDatas();
         console.log("認証成功");
       } else {
@@ -109,13 +110,14 @@ const Sub: React.FC = () => {
   };
   const checksetC = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
-    // 正しい正規表現を使用
+    // 半角文字のみの入力しか受け付けず、半角で文字数を4文字に設定
     if (/^\d{0,4}$/.test(value)) {
       setTextC(value);
     }
   };
   const checksetN = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
+    //全角、半角関係なく文字数を30文字に制限
     if (/^.{0,30}$/.test(value)) {
       setTextN(value);
     }
@@ -123,6 +125,8 @@ const Sub: React.FC = () => {
   const back = () => {
     navigate("/");
   };
+
+  //ステートに画像ファイルを保管、スイッチを切り替えたときに更新値に他の画像を代入し切り替える
   const changepic = () => {
     if (pic === nsx) {
       setpic(legend);
@@ -133,30 +137,32 @@ const Sub: React.FC = () => {
   return (
     <div
       style={{
-        backgroundImage: `url(${pic})`,
+        backgroundImage: `url(${pic})`, //picに保管されている現状値を背景画像に設定
         backgroundSize: "cover", // 背景画像をコンテナに合わせてサイズ調整
         backgroundPosition: "center", // 背景画像を中央に配置
         height: "100vh", // コンテナの高さを画面全体に設定
-        width: "100vw",
+        width: "100vw", //コンテナの横幅を画面全体に設定
       }}
     >
       <Grid
-        item
-        xs={11}
+        item 
+        xs={11} //画面幅の比率を設定
         container
-        direction="column"
-        alignItems="center"
-        justifyContent="center"
+        direction="column" //位置調整の際、縦方向に調整する
+        alignItems="center" //子要素を中央ぞろえにする
+        justifyContent="center" //子要素を画面中央に配置
       >
-        <motion.h1 initial={{ x: "-100%" }}
+        <motion.h1 initial={{ x: "-100%" }} /*アニメーション設定*/
         animate={{ x: 100 }}
         transition={{ duration: 1 }}style={{ marginTop: "80px", color: h1 }}>
           コード:4ケタ以内半角数字
         </motion.h1>
+
         <motion.h1  animate={{ x: 100 }}
         transition={{ duration: 1 }} style={{  color: h1 }}>
           名前:30文字以内
         </motion.h1>
+
         <Box style={{ marginTop: "150px" }}>
           <Box
             display="flex"
@@ -195,6 +201,7 @@ const Sub: React.FC = () => {
             justifyContent="center"
             style={{ marginTop: "30px" }}
           >
+  
             <Button
               variant="contained"
               size="large"
@@ -203,6 +210,7 @@ const Sub: React.FC = () => {
             >
               戻る
             </Button>
+
             <Button
               variant="contained"
               size="large"
@@ -210,6 +218,7 @@ const Sub: React.FC = () => {
             >
               {isEdit ? "登録" : "更新"}
             </Button>
+
             {!isEdit && (
               <Button
                 variant="contained"
@@ -223,6 +232,7 @@ const Sub: React.FC = () => {
                 削除
               </Button>
             )}
+            
             <Switch onChange={changepic} color="warning" />
           </Box>
         </Box>
@@ -249,12 +259,14 @@ const Sub: React.FC = () => {
           >
             キャンセル
           </Button>
+
           <Button onClick={handlePasswordSubmit} color="primary">
             送信
           </Button>
+
         </DialogActions>
       </Dialog>
-
+      
       {isOK && (
         <div>
           <p>削除が認証されました</p>
